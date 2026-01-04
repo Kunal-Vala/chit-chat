527985e0cfffde33b09b42377921d63239350aaf
Kunal-Vala <kunalvala2005@gmail.com>
2026-01-04 10:55:25 +0530

    feat : sign in route

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,9 +1,9 @@
-import  Express  from "express";
-import { sign_up } from "../controllers/auth.controller";
+import Express from "express";
+import { sign_up, sign_in } from "../controllers/auth.controller";
 
 export const router = Express.Router();
 
-router.post('/sign_up',sign_up);
-// router.post('/sign_in');
+router.post('/sign_up', sign_up);
+router.post('/sign_in', sign_in);
 
 
