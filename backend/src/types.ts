787f2f9c258ebb3409f1e84f479f8e7fe7d9d3f5
Kunal-Vala <kunalvala2005@gmail.com>
2026-01-21 21:51:37 +0530

    add (types) : messages,Conversation

--- backend/src/types.ts
+++ backend/src/types.ts
@@ -35,3 +35,48 @@ export interface SignInBody {
   email: string;
   password: string;
 }
+
+
+export interface IConversation {
+  _id: string;
+  participants: string[];
+  type: 'direct' | 'group'
+  groupName?: string
+  groupAvatarUrl?: string;
+  groupDescription?: string
+  members?: Array<{
+    userId: string;
+    role: 'admin' | 'member';
+    joinedAt?: Date;
+  }>;
+  unreadCount?: { [userId: string]: number };
+  adminId?: string
+  lastMessage?: string
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+export interface IMessage {
+  _id: string;
+  conversationId: string;
+  senderId: string;
+  content: string;
+  messageType: 'text' | 'image' | 'file';
+  mediaUrl?: string;
+  deliveryStatus: 'sent' | 'delivered' | 'read';
+  sentAt: Date;
+  deliveredAt?: Date;
+  readAt?: Date;
+  readBy?: string[];
+  isEdited?: boolean;
+  editedAt?: Date;
+  isDeleted?: boolean;
+  deletedAt?: Date;
+  replyToMessageId?: string;
+  reactions?: Array<{
+    emoji: string;
+    userId: string;
+  }>;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
