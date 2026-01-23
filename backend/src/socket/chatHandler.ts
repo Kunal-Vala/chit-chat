4abcffb218c08c823e2a3366e14d55ccd83d3f6d
Kunal-Vala <kunalvala2005@gmail.com>
2026-01-23 20:45:40 +0530

    add : middleware for auth

--- backend/src/socket/chatHandler.ts
+++ backend/src/socket/chatHandler.ts
@@ -1,17 +1,45 @@
 import { Server, Socket } from "socket.io";
-import { Jwt } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import Message from "../models/Message";
 import Conversation from "../models/Conversation";
 import { JwtPayloadType } from "../types";
 import User from "../models/User";
+import { JWT_SECRET } from "../config/env";
 
+
+// Store online users: userId -> socketId
 const onlineUsers = new Map<string, string>();
 
-export const setupChatHandlers = (io : Server) => {
 
-}
-
-
-export const getOnlineUsers = () =>{
+export const getOnlineUsers = () => {
     return Array.from(onlineUsers.keys());
 };
+
+
+
+export const setupChatHandlers = (io: Server) => {
+
+    // Socket.io middleware for Authentication 
+
+    io.use((socket, next) => {
+
+        const token = socket.handshake.auth.token;
+
+        if (token) {
+            return next(new Error("Authentication Failed"));
+        }
+
+        try {
+            const decoded = jwt.verify(token, JWT_SECRET) as JwtPayloadType;
+            socket.data.userId = decoded.userId;
+            socket.data.username = decoded.username;
+            next();
+
+        } catch (err) {
+            next(new Error(err + "Authentication Error"));
+        }
+    });
+
+    
+}
+
