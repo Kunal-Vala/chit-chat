a1655981591fe17030dc5674b5e7167f5efd09a5
Kunal-Vala <kunalvala2005@gmail.com>
2026-01-24 20:53:39 +0530

    add: getConversation , createConversation

---
+++ backend/src/controllers/chat.controller.ts
@@ -0,0 +1,74 @@
+import { Response } from "express";
+import { AuthenticatedRequest } from "../types";
+import Conversation from "../models/Conversation";
+import Message from "../models/Message";
+import User from "../models/User";
+
+// GET api/chat/conversation - get all user's conversation
+
+export const getUserConversations = async (req: AuthenticatedRequest, res: Response) => {
+    try {
+        const userId = req.user?.userId;
+
+        const conversation = await Conversation.find({
+            participants: userId,
+        })
+            .populate('participants', 'username profilePictureUrl onlineStatus lastSeen')
+            .populate({
+                path: 'lastMessageId',
+                select: 'content messageType sentAt senderId'
+            })
+            .sort({ updatedAt: -1 });
+
+        res.json({ conversation });
+    } catch (error) {
+        console.error('Error Fetching Conversation', error);
+        res.status(500).json({ error: 'Failed to fetch conversation' });
+    }
+};
+
+// POST api/chat/conversation - create new conversation
+
+export const createConversation = async (req: AuthenticatedRequest, res: Response) => {
+    try {
+        const userId = req.user?.userId;
+        const { participantId } = req.body;
+
+        if (!participantId) {
+            return res.status(400).json({ error: 'Participant Id is required' });
+
+        }
+
+        const existingConversation = await Conversation.findOne({
+            conversationType: 'direct',
+            participants: { $all: [userId, participantId] }
+
+        });
+
+        if (existingConversation) {
+            return res.json({ conversation: existingConversation });
+        }
+
+        // verify both user exist
+
+        const otherUser = await User.findById(participantId);
+
+        if (!otherUser) {
+            return res.status(404).json({ error: 'user does not exist' });
+        }
+
+        const conversation = await Conversation.create({
+            participants: [userId, participantId],
+            conversationType: 'direct'
+
+        });
+
+        await conversation.populate('participants', 'username profilePictureUrl onlineStatus');
+
+        return res.status(201).json({ conversation });
+
+    } catch (error) {
+        console.error('Error Creating Conversation', error);
+        return res.status(500).json({ error: 'Failed To Create Conversation' });
+    }
+};
