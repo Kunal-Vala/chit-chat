7054a3cc8798a41b5dec527bae3deab49fbe6af2
Kunal-Vala <kunalvala2005@gmail.com>
2026-01-25 16:35:28 +0530

    add : get specific conversation

--- backend/src/controllers/chat.controller.ts
+++ backend/src/controllers/chat.controller.ts
@@ -3,6 +3,7 @@ import { AuthenticatedRequest } from "../types";
 import Conversation from "../models/Conversation";
 import Message from "../models/Message";
 import User from "../models/User";
+import { error } from "node:console";
 
 // GET api/chat/conversation - get all user's conversation
 
@@ -70,5 +71,34 @@ export const createConversation = async (req: AuthenticatedRequest, res: Respons
     } catch (error) {
         console.error('Error Creating Conversation', error);
         return res.status(500).json({ error: 'Failed To Create Conversation' });
+    }
+};
+
+// GET api/chat/conversation/:conversationId - get specific conversation
+
+export const getConversation = async (req: AuthenticatedRequest, res: Response) => {
+
+    try {
+        const userId = req.user?.userId;
+        const { conversationId } = req.params;
+
+        if (!conversationId) {
+            return res.status(400).json({ error: 'Conversation Id Required' });
+        }
+
+        const conversation = await Conversation.findOne({
+            _id: conversationId,
+            participants: userId,
+        }).populate('participants', 'username profilePictureUrl onlineStatus lastSeen');
+
+        if (!conversation) {
+            return res.status(400).json({ error: 'Conversation not found' });
+        }
+
+        res.json({ conversation });
+
+    } catch (error) {
+        console.error('Error fetching conversation:', error);
+        res.status(500).json({ error: 'Failed to fetch conversation' });
     }
 };
