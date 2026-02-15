8b4d04722f87b22ae9a8de4c188ee13d94e8f937
Kunal-Vala <kunalvala2005@gmail.com>
2026-02-15 20:19:00 +0530

    change: port number

--- frontend/src/api/userApi.ts
+++ frontend/src/api/userApi.ts
@@ -2,7 +2,7 @@ import axios, { AxiosError } from "axios";
 import { getStoredToken } from "./axiosConfig";
 import type { UserProfile, FriendRequest } from "../types";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ?? 'http://localhost:3000/api'
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ?? 'http://localhost:5000/api'
 
 /**HELPER TO EXTRACT ERROR MESSAGE FROM API RESPONSE */
 
